da79e7f1abd84a35000f1d5ad2662edfa65c7e01
Pob <590650@gmail.com>
2018-09-09 22:55:04 +0700

    re config ESLint with react-app and prettier config

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,3 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => <div>Comment Box</div>
+class CommentBox extends Component {
+  state = {
+    comment: ''
+  }
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value })
+  }
+
+  render() {
+    const { comment } = this.state
+
+    return (
+      <form>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={comment} />
+        <div>
+          <button type="submit">Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default CommentBox
